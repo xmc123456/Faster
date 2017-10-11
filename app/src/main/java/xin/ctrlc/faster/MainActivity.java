729ea63f3f77b4db4036d8d91ff2dedf3e3d961d
wangxin <912897493@qq.com>
2017-10-11 09:54:41 +0800

    first commit

--- app/src/main/java/xin/ctrlc/faster/MainActivity.java
+++ app/src/main/java/xin/ctrlc/faster/MainActivity.java
@@ -2,6 +2,7 @@ package xin.ctrlc.faster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
 
-    private void aaaa(){
-
+    private void initView(){
+        Log.d("MainActivity","init view ...");
     }
 }
